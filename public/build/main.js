a6db916eb868106a0cb5a8674cb119a76f49306e
daniuribe25 <dani.uribe25@gmail.com>
2017-10-23 14:57:25 -0500

    Update main.js

--- public/build/main.js
+++ public/build/main.js
@@ -200,7 +200,7 @@ var Reviews = (function () {
             return Promise.resolve(this.data);
         }
         return new Promise(function (resolve) {
-            _this.http.get('https://ionicnode-final.herokuapp.com/api/reviews')
+            _this.http.get('https://ionicnode-final.herokuapp.com/pruebaz')
                 .map(function (res) { return res.json(); })
                 .subscribe(function (data) {
                 alert(data);
